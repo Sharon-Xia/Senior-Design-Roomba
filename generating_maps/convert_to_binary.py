21de39baf59e7176e2d64304c375004347a5faff
Sharon-Xia <56088644+Sharon-Xia@users.noreply.github.com>
2021-11-09 00:23:17 -0800

    square map created

--- generating_maps/convert_to_binary.py
+++ generating_maps/convert_to_binary.py
@@ -1,7 +1,7 @@
 import cv2 # Import OpenCV
    
 # read the image file
-img = cv2.imread('mii_floor_plan_4.png')
+img = cv2.imread('square_map.png')
    
 ret, bw_img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)
    
@@ -10,6 +10,6 @@ bw = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
   
 # Display and save image 
 cv2.imshow("Binary", bw_img)
-cv2.imwrite("mii_floor_plan_4_binary.png", bw_img)
+cv2.imwrite("square_map.png", bw_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
