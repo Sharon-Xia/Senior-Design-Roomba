218cc1cdef1a099a09b59b7b905fd0f8da016ec5
Sharon Xia <shxia9924456@gmail.com>
2021-11-09 00:13:40 -0800

    map-making beginning

--- src/beginner_tutorials/scripts/safety_node.py
+++ src/beginner_tutorials/scripts/safety_node.py
@@ -14,12 +14,12 @@ class Safety(object):
     """
     The class that handles emergency braking.
     """
-    safety_threshold = 10 # ttc threshold
+    safety_threshold = .7 # ttc threshold
 
-    scan_sub_rate = 200 # once every 50 msgs
+    scan_sub_rate = 100 # once every 50 msgs
     scan_sub_tick = 0
 
-    odom_sub_rate = 200
+    odom_sub_rate = 100
     odom_sub_tick = 0
 
     def __init__(self):
@@ -52,13 +52,19 @@ class Safety(object):
         if self.odom_sub_tick >= self.odom_sub_rate:
             rospy.loginfo("twist.linear.x, y, z: %f , %f, %f", odom_msg.twist.twist.linear.x, odom_msg.twist.twist.linear.y, odom_msg.twist.twist.linear.z)
             rospy.loginfo("twist.angular.x, y, z: %f , %f, %f", odom_msg.twist.twist.angular.x, odom_msg.twist.twist.angular.y, odom_msg.twist.twist.angular.z)
-            rospy.loginfo("pose.position.x, y, z: %f , %f, %f", odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y, odom_msg.pose.pose.position.z)
+            # rospy.loginfo("pose.position.x, y, z: %f , %f, %f", odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y, odom_msg.pose.pose.position.z)
             rospy.loginfo("pose.orientation.x, y, z: %f , %f, %f", odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y, odom_msg.pose.pose.orientation.z)
             
             self.speed = sqrt(odom_msg.twist.twist.linear.x ** 2 + (odom_msg.twist.twist.linear.y ** 2))
+            if odom_msg.twist.twist.linear.x < 0: 
+                self.speed *= -1
             rospy.loginfo("current speed: %f", self.speed)
 
             self.odom_sub_tick = 0
+
+    # only check for collisions if angle is within certain scope
+    def is_in_scope(self, theta):
+        return (theta < (pi/4) and theta > (-pi/4)) or (theta > (pi * 3/4) or theta < (pi * -3/4))
 
 
     def scan_callback(self, scan_msg):
@@ -75,7 +81,7 @@ class Safety(object):
                 theta = scan_msg.angle_min + (i * scan_msg.angle_increment)
                 rd = self.speed * cos(theta) # rderivative, or vcostheta
                 #rospy.loginfo("theta: %f, rd: %f", theta, rd)
-                if (rd > 0):
+                if (rd > 0 and self.is_in_scope(theta)):
                     ttc = d / (rd)
                     if (ttc < TTC):
                         TTC = ttc # ttc's
@@ -86,7 +92,7 @@ class Safety(object):
 
             rospy.loginfo("TTC: %f", TTC)
             rospy.loginfo("TTC_theta: %f", ttc_theta)
-            rospy.loginfo("TTC_rd: %f", ttc_rd)
+            # rospy.loginfo("TTC_rd: %f", ttc_rd)
             # rospy.loginfo(ttc_range)            
 
             # TODO: publish brake message and publish controller bool
